68b04fdc771fcb3954f09139913c947b62f915ae
@eguzman2 <eguzman@buk.cl>
2021-12-19 12:54:57 -0300

    feat: manejo de estado

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/product_item.dart';
+import '../widgets/products_grid.dart';
+import '../models/product.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: ProductGrid(),
+    );
+  }
+}
